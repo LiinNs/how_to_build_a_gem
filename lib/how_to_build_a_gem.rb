a85e8bacca9bceb857b6425c84c99d282182d452
LiinNs <876056609@qq.com>
2019-04-30 11:06:01 +0800

    init

---
+++ lib/how_to_build_a_gem.rb
@@ -0,0 +1,6 @@
+require "how_to_build_a_gem/version"
+
+module HowToBuildAGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/how_to_build_a_gem/version.rb
@@ -0,0 +1,3 @@
+module HowToBuildAGem
+  VERSION = "0.1.0"
+end
